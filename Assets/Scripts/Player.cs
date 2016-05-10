40dc3da802b33e06f1e2f5fe1dcec823136140e8
vitalya kvas <vitalya kvas>
2016-05-10 22:10:09 +0300

    Controlling player.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	public Vector2 jumpForce = new Vector2(0, 300);
+
+	void Jump()
+	{
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        GetComponent<Rigidbody2D>().AddForce(jumpForce);
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyUp ("space"))
+			Jump ();
+	}
+}
