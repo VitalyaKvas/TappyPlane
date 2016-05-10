154bc3a640b5aa23a38d5b741837d93201ce7758
vitalya kvas <vitalya kvas>
2016-05-10 23:26:02 +0300

    Generate obstacles.

---
+++ Assets/Scripts/Generate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Generate : MonoBehaviour
+{
+    public List<GameObject> rocks;
+    public Transform ParentRocks;
+
+    void Start()
+    {
+        InvokeRepeating("GenerateObstacles", 2.5f, 2.5f);
+    }
+
+    void GenerateObstacles()
+    {
+        var randomIndex = Random.Range(0, rocks.Count);
+        var rock = rocks[randomIndex];
+
+        var newRock = Instantiate(rock, new Vector3(45, 0, 0), Quaternion.identity) as GameObject;
+        newRock.transform.parent = ParentRocks;
+    }
+}
