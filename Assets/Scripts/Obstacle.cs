aedd11526c1b1841c2a46c38b395cf1eecce1aef
vitalya kvas <vitalya kvas>
2016-05-10 22:15:59 +0300

    Moving obstacles.

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public Vector2 velocity = new Vector2(-4, 0);
+    
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = velocity;
+    }
+}
